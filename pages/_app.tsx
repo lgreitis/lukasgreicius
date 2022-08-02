2befb96b7c861591ff7ab6679fe9560bef6d394b
lgreitis <lukas.greicius@ktu.edu>
2022-08-02 17:19:05 +0300

    feat: SEO optimization, remove unused file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,11 @@
-import '../styles/globals.css';
+import '../styles/globals.css'
 
-import { CacheProvider, EmotionCache } from '@emotion/react';
-import { CssBaseline, ThemeProvider } from '@mui/material';
-import Head from 'next/head';
+import {CacheProvider, EmotionCache} from '@emotion/react'
+import {CssBaseline, ThemeProvider} from '@mui/material'
+import Head from 'next/head'
 
-import createEmotionCache from '../src/utils/createEmotionCache';
-import theme from '../styles/theme/theme';
+import createEmotionCache from '../src/utils/createEmotionCache'
+import theme from '../styles/theme/theme'
 
 import type {AppProps} from 'next/app'
 // Client-side cache, shared for the whole session of the user in the browser.
@@ -20,6 +20,11 @@ export default function MyApp(props: MyAppProps) {
   return (
     <CacheProvider value={emotionCache}>
       <Head>
+        <title>Lukas Greičius</title>
+        <meta
+          name='description'
+          content='Personal page of Lukas Greičius.'
+        ></meta>
         <meta name='viewport' content='initial-scale=1, width=device-width' />
       </Head>
       <ThemeProvider theme={theme}>
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,14 +5,11 @@ import {
   IconBrandSpotify,
 } from '@tabler/icons'
 
-// import SpotifyCard from '../src/components/spotifyCard'
-
 import type {NextPage} from 'next'
 
 const Home: NextPage = () => {
   return (
     <>
-      {/* <SpotifyCard /> */}
       <Grid
         container
         direction='column'
@@ -27,13 +24,19 @@ const Home: NextPage = () => {
           Full Stack TypeScript developer
         </Typography>
         <Grid item sx={{pt: 2}}>
-          <IconButton href='https://www.linkedin.com/in/lukas-grei%C4%8Dius-609955217/'>
+          <IconButton
+            title='LinkedIn'
+            href='https://www.linkedin.com/in/lukas-grei%C4%8Dius-609955217/'
+          >
             <IconBrandLinkedin size='32' />
           </IconButton>
-          <IconButton href='https://github.com/lgreitis/'>
+          <IconButton title='Github' href='https://github.com/lgreitis/'>
             <IconBrandGithub size='32' />
           </IconButton>
-          <IconButton href='https://open.spotify.com/playlist/51IERYOUUwMAimdgaRE2Si?si=274b38785a7b4a62'>
+          <IconButton
+            title='Spotify Playlist'
+            href='https://open.spotify.com/playlist/51IERYOUUwMAimdgaRE2Si?si=274b38785a7b4a62'
+          >
             <IconBrandSpotify size='32' />
           </IconButton>
         </Grid>
