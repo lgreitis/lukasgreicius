9b4ad528f49f40d4280c2286ec9c8076c9a3673e
lgreitis <lukas.greicius@ktu.edu>
2023-10-30 10:24:48 +0200

    fix: mobile, structure rework

--- src/modules/work/mobile/workMobile.tsx
+++ src/modules/work/mobile/workMobile.tsx
@@ -26,24 +26,29 @@ const WorkMobile = () => {
           onClose={() => setSelectedItem(null)}
         >
           <Drawer.Portal>
-            <Drawer.Content className="fixed bottom-0 left-0 right-0 mt-24 flex  max-h-[96%] flex-col rounded-t-[10px] bg-black">
-              <img
-                className="w-full rounded-t-3xl object-cover"
-                src={selectedItem.image}
-              />
-              <CardHeaderItems
-                card={selectedItem}
-                className="rounded-b-3xl bg-black"
-                animate={false}
-              />
-              <CardTech card={selectedItem} animate={false} />
-              <div
-                className={cn(
-                  "prose prose-invert max-w-none overflow-auto rounded-b-3xl bg-black px-6",
-                  selectedItem.description && "pb-6",
-                )}
-              >
-                {selectedItem.description}
+            <Drawer.Content className="fixed bottom-0 left-0 right-0 mt-24 flex max-h-[96%] flex-col rounded-t-3xl bg-black outline-none">
+              <div className="relative">
+                <img
+                  className="z-10 w-full rounded-t-3xl object-cover"
+                  src={selectedItem.image}
+                />
+                <div className="absolute left-0 top-0 h-full w-full"></div>
+              </div>
+              <div className="overflow-auto">
+                <CardHeaderItems
+                  card={selectedItem}
+                  className="rounded-b-3xl bg-black"
+                  animate={false}
+                />
+                <CardTech card={selectedItem} animate={false} />
+                <div
+                  className={cn(
+                    "prose prose-invert max-w-none rounded-b-3xl bg-black px-6",
+                    selectedItem.description && "pb-6",
+                  )}
+                >
+                  {selectedItem.description}
+                </div>
               </div>
             </Drawer.Content>
             <Drawer.Overlay />
--- src/modules/work/work.tsx
+++ src/modules/work/work.tsx
@@ -1,29 +1,11 @@
-import { Dialog } from "@headlessui/react";
-import { XMarkIcon } from "@heroicons/react/20/solid";
-import { AnimatePresence, motion } from "framer-motion";
-import { useState } from "react";
+import React from "react";
 import { BrowserView, MobileOnlyView } from "react-device-detect";
-import Button from "~/components/button/button";
-import { usePrevious } from "~/hooks/usePrevious";
-import CardHeader from "~/modules/work/components/CardHeader";
-import CardTech from "~/modules/work/components/CardTech";
+import WorkDesktop from "~/modules/work/desktop/workDesktop";
 import WorkMobile from "~/modules/work/mobile/workMobile";
-import { CardItem, workItems } from "~/modules/work/workItems";
-import { cn } from "~/utils/cn";
 
 const Work = () => {
-  const [selectedItem, setSelectedItem] = useState<CardItem | null>(null);
-  // Fixing z indexes when card is being closed with usePrevious
-  const prevSelectedId = usePrevious(selectedItem?.id);
-  const [cardExpanded, setCardExpanded] = useState(false);
-
-  const closeModal = () => {
-    setSelectedItem(null);
-    setCardExpanded(false);
-  };
-
   return (
-    <>
+    <React.Fragment>
       <div>
         <div className="flex justify-center px-6 pb-6">
           <div className="grid h-full max-w-screen-xl grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
@@ -31,92 +13,12 @@ const Work = () => {
               <WorkMobile />
             </MobileOnlyView>
             <BrowserView className="contents">
-              {workItems.map((item) => (
-                <motion.div
-                  className={cn(
-                    "relative flex cursor-pointer flex-col overflow-hidden rounded-3xl border border-neutral-800 bg-black transition-colors hover:bg-neutral-900",
-                    prevSelectedId === item.id && "z-10",
-                  )}
-                  key={item.id}
-                  onClick={() => setSelectedItem(item)}
-                  layoutId={`card-container-${item.id}`}
-                >
-                  <motion.img
-                    layoutId={`card-image-${item.id}`}
-                    className="w-full object-cover"
-                    src={item.image}
-                  />
-                  <CardHeader card={item} />
-                  <CardTech card={item} />
-                </motion.div>
-              ))}
+              <WorkDesktop />
             </BrowserView>
           </div>
         </div>
-
-        <AnimatePresence>
-          <Dialog
-            as="div"
-            open={selectedItem !== null}
-            onClose={closeModal}
-            className="relative z-20"
-          >
-            {/* TODO: figure out why Transition doesnt work */}
-            <div className="fixed inset-0 bg-black bg-opacity-50" />
-
-            <div className="fixed inset-0 w-full overflow-y-auto overflow-x-hidden">
-              <div className="flex min-h-full items-center justify-center">
-                {selectedItem && (
-                  <Dialog.Panel
-                    as={motion.div}
-                    className="relative max-h-screen w-full rounded-3xl bg-black md:w-11/12 lg:w-9/12 xl:w-7/12"
-                    layoutId={`card-container-${selectedItem.id}`}
-                  >
-                    <motion.img
-                      layoutId={`card-image-${selectedItem.id}`}
-                      className="w-full rounded-t-3xl object-cover"
-                      src={selectedItem.image}
-                    />
-                    <CardHeader
-                      card={selectedItem}
-                      className="rounded-b-3xl bg-black"
-                    />
-                    <CardTech card={selectedItem} />
-                    <motion.button
-                      className="absolute right-5 top-5 rounded-full bg-black bg-opacity-40 p-1 text-white"
-                      onClick={closeModal}
-                    >
-                      <XMarkIcon className="w-5" />
-                    </motion.button>
-                    {!cardExpanded && selectedItem.description && (
-                      <div className="flex items-center pb-4">
-                        <Button
-                          type="button"
-                          onClick={() => setCardExpanded(true)}
-                          className="mx-auto"
-                        >
-                          Read more
-                        </Button>
-                      </div>
-                    )}
-                    {cardExpanded && (
-                      <motion.div
-                        className={cn(
-                          "prose prose-invert max-w-none rounded-b-3xl bg-black px-6",
-                          selectedItem.description && "pb-6",
-                        )}
-                      >
-                        {selectedItem.description}
-                      </motion.div>
-                    )}
-                  </Dialog.Panel>
-                )}
-              </div>
-            </div>
-          </Dialog>
-        </AnimatePresence>
       </div>
-    </>
+    </React.Fragment>
   );
 };
 
